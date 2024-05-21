5a6b9f296d69eb2c317ec3613886468b87b7429f
Michael-Andryeer <andryeerm@gmail.com>
2024-05-20 21:10:39 -0300

    Posições e direções

---
+++ ChessLogic/Player.cs
@@ -0,0 +1,28 @@
+﻿
+namespace ChessLogic
+{
+  public enum Player
+    {
+        None,
+        White,
+        Black
+    }
+
+    public static class PlayerExtesions
+    {
+        public static Player Opponent(this Player player)
+        {
+            switch(player)
+            {
+                case Player.White:
+                    return Player.Black;
+                case Player.Black:
+                    return Player.White;
+                default:
+                    return Player.None;
+
+            }
+        }
+
+    }
+}
